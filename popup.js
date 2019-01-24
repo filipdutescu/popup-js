89d52f939c23a0aa9f5921e0f96d7f89c2315014
Filip Dutescu <filip.dutescu@gmail.com>
2019-01-24 12:10:37 +0200

    work on script

--- popup.js
+++ popup.js
@@ -1,14 +1,26 @@
-function Dialog()
+function Popup(id)
 {
+    if(typeof jQuery == 'undefined'){
+        var jq = document.createElement("script");
+        jq.type = "text/javascript";
+        jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+        document.head.appendChild(jq);
+    }
 
+    this.id = '#' + id;
+
+    $(this.id + " .close").on('click', function () {
+        $(this.id).css("opacity", "0");
+        $(this.id).css("top", "-50%");        
+    });
 }
 
-function create(dialogId) {
-    $(dialogId).css("opacity", "1");
-    $(dialogId).css("top", "30%");
+function createPopup(id) {
+    $(id).css("opacity", "1");
+    $(id).css("top", "30%");
 }
 
-function closeDialog() {
-    $("#creation-dialog").css("opacity", "0");
-    $("#creation-dialog").css("top", "-50%");
-}
+// function closeDialog() {
+//     $("#creation-dialog").css("opacity", "0");
+//     $("#creation-dialog").css("top", "-50%");
+// }
