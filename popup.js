8fe34214359e8870d4c107521ca7810074b59e27
Filip Dutescu <filip.dutescu@gmail.com>
2019-01-28 18:01:51 +0200

    added public default options

--- popup.js
+++ popup.js
@@ -5,45 +5,33 @@ function togglePopup(id) {
 
 (function($) {
     $.fn.popup = function(options) {
-        var settings = $.extend({
-            title: "Popup Title",
-            content: "Popup Content",
-
-            hidden: true,
-            header: true,
-            
-            theme: "modern",
-            
-            controls: {
-                close: true
-            }
-        }, $.fn.popup.defaults, options);
+        var settings = $.extend({}, $.fn.popup.defaults, options);
         
-        this.addClass("popup");
+        this.addClass(settings.style.classes.main);
         if(settings.hidden === true)
-            this.addClass("popup-hidden");
+            this.addClass(settings.style.classes.hidden);
         
         var header = `
-        ${settings.theme === "modern" ? settings.controls.close === true ? "<div style='position:relative;'><button class='popup-ctrl' style='position: absolute; right: -5px; top: -5px;'>&#215;</button></div>" : "" : ""}
-        <div class='popup-header'>
+        ${settings.theme === "modern" ? settings.controls.close === true ? `<div style='position:relative;'><button class='${settings.style.classes.control}' style='position: absolute; right: -5px; top: -5px;'>&#215;</button></div>` : "" : ""}
+        <div class='${settings.style.classes.header}'>
         ${settings.title}
         <hr />
         </div>
         `;
         
         var body = `
-        <div class='popup-body'>
+        <div class='${settings.style.classes.body}'>
             ${settings.content}
         </div>
         `;
         
         var controls = `
-        <div class='popup-controls'>
-        ${settings.theme === "classic" ? settings.controls.close === true ? "<button class='popup-ctrl' style='position: absolute; right: 10px; top: 10px;'>&#215;</button>" : "" : ""}
+        <div class='${settings.style.classes.controls}'>
+        ${settings.theme === "classic" ? settings.controls.close === true ? `<button class='${settings.style.classes.control}' style='position: absolute; right: 10px; top: 10px;'>&#215;</button>` : "" : ""}
         </div>
         `;
 
-        this.html(settings.header ? header : "" + body + controls);
+        this.html((settings.header ? header : "") + body + controls);
         
         if (settings.controls.close === true) {
             var popup = this;
@@ -58,4 +46,25 @@ function togglePopup(id) {
 }(jQuery));
 
 $.fn.popup.defaults = {
+    title: "Popup Title",
+    content: "Popup Content",
+
+    hidden: true,
+    header: true,
+    
+    theme: "modern",
+    
+    controls: {
+        close: true
+    },
+    style: {
+        classes: {
+            main: "popup",
+            hidden: "popup-hidden",
+            header: "popup-header",
+            body: "popup-body",
+            controls: "popup-controls",
+            control: "popup-ctrl",
+        }
+    }
 };
